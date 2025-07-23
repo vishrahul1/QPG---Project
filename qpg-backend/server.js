a2b94932e425d14b64fc749a826c362af30b9c50
vishrahul1 <turnerevent.co@gmail.com>
2025-07-24 01:15:15 +0530

    Initial commit for QPG backend

---
+++ qpg-backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB(); // connect to MongoDB
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); // for JSON body
+app.use(express.urlencoded({ extended: true })); // for forms
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('🚀 QPG API is Running');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
